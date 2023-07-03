0612a8d1fe9243e42527a692c3b98b49b580e5ae
Talha Mujahid <htmujahid@gmail.com>
2023-07-03 09:49:35 +0500

    Update nodemailer.ts

--- server/config/nodemailer.ts
+++ server/config/nodemailer.ts
@@ -3,7 +3,6 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
-    secure: true,
     auth: {
         user: process.env.MAIL_USERNAME,
         pass: process.env.MAIL_PASSWORD
