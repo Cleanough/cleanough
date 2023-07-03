3f50904d36635554e542c4b2da069041f23901a6
Talha Mujahid <htmujahid@gmail.com>
2023-07-03 09:26:52 +0500

    Update route.tsx

--- app/api/auth/forgot-password/route.tsx
+++ app/api/auth/forgot-password/route.tsx
@@ -63,7 +63,7 @@ export async function POST(request: CustomNextRequest, { params }: Params) {
         html: render(<ForgotPasswordTemplate token={token} />)
     };
 
-    await transporter.sendMail(mailOptions, (error: any, info: any) => {
+    transporter.sendMail(mailOptions, (error: any, info: any) => {
         if (error) {
             return response.json(
                 { error: status[status.BAD_GATEWAY] },
@@ -73,7 +73,7 @@ export async function POST(request: CustomNextRequest, { params }: Params) {
                 }
             );
         }
-        return NextResponse.json({ route: "test"});
+        console.log(info)
     });
-
+    return NextResponse.json({ route: "Forgot Password"});
 }
